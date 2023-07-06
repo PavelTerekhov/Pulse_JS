c48d6f5c6a63c72ab8c5232a46c27ecdf5e7ba0d
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-07-06 23:50:12 +0700

    added promo.sass and project setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const gulp        = require('gulp')
+const browserSync = require('browser-sync')
+const sass        = require('gulp-sass')(require('sass'))
+const cleanCSS = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require("gulp-rename")
 
 gulp.task('server', function() {
     browserSync({
@@ -12,10 +12,10 @@ gulp.task('server', function() {
         },
         browser: 'chrome',
         notify: false
-    });
+    })
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+    gulp.watch("src/*.html").on('change', browserSync.reload)
+})
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
@@ -24,11 +24,11 @@ gulp.task('styles', function() {
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+        .pipe(browserSync.stream())
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'))
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
