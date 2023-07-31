423250805c33381477b529a8cb45de0087dc793a
Pavel <pavel.a.terekhov@gmail.com>
2023-07-31 17:22:21 +0700

    Tiny Slider finish

--- src/js/script.js
+++ src/js/script.js
@@ -16,10 +16,33 @@
 //       });
 //   });
 
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
-    controlsText: ['icons/left.svg', 'icons/right.svg']
+    controls: false,
+    nav: fase,
+    // responsive: {
+    //     640: {
+    //       edgePadding: 20,
+    //       gutter: 20,
+    //       items: 1
+    //     },
+    //     700: {
+    //       gutter: 30
+    //     },
+    //     900: {
+    //       items: 3
+    //     }
+    //   }
   });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo("prev");
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo("next");
+});
+  
