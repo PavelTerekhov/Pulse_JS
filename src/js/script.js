a319c90d1f5556938509e145747d2466e83d1efb
Pavel <pavel.a.terekhov@gmail.com>
2023-07-31 17:06:24 +0700

    Tiny Slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrow: false
-                }
-            }
-        ]
-      });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     dots: true,
+//                     arrow: false
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controlsText: ['icons/left.svg', 'icons/right.svg']
   });
