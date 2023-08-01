f90edabfc2c014067b3c02fe5a3ac2b5fe50db48
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-08-01 22:12:58 +0700

    Adedd tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,42 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrow: false
-                }
-            }
-        ]
-      });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     dots: true,
+//                     arrow: false
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controlsText: ['icons/left.svg', 'icons/right.svg'],
+    controls: false,
+    nav: false,
+    // responsive: {
+    //     640: {
+    //       edgePadding: 20,
+    //       gutter: 20,
+    //       items: 1
+    //     },
+    //     700: {
+    //       gutter: 30
+    //     },
+    //     900: {
+    //       items: 3
+    //     }
+    //   }
   });
 
 document.querySelector('.prev').addEventListener('click', function () {
