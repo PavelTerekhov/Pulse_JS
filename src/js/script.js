17ce650320ff109f7b31870811f01f6d8dc3de70
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-08-06 01:00:16 +0700

    Tabs finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,53 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     dots: true,
-//                     arrow: false
-//                 }
-//             }
-//         ]
-//       });
-//   });
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controlsText: ['icons/left.svg', 'icons/right.svg'],
-    controls: false,
-    nav: false,
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
+
+
+
+
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controlsText: ['icons/left.svg', 'icons/right.svg'],
+//     controls: false,
+//     nav: false,
     // responsive: {
     //     640: {
     //       edgePadding: 20,
@@ -37,13 +61,13 @@ const slider = tns({
     //       items: 3
     //     }
     //   }
-  });
+//   });
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo("prev");
-});
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo("prev");
+// });
 
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo("next");
-});
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo("next");
+// });
   
